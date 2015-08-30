0ed89ec8ab1f419e28e3731f8f79102de9e1edf2
liv123 <olivia.e@gmail.com>
2015-08-30 15:07:27 -0700

    added html and began routing

--- index.js
+++ index.js
@@ -2,20 +2,29 @@
 var express = require('express'),
     app = express(),
     path = require('path'),
-    bodyParser = require('bodyParser'),
+    bodyParser = require('body-parser'),
     _=require('underscore'),
     //var db below plugs databse in the express app//
-    db = require("./models"),
+    //db = require("./models"),
     views = path.join(process.cwd(), "views/");
 
+// DATA //
+var phrases =[
+  {word: "sql", definition: "A special-purpose programming language designed for managing data held in a RDBMS."},
+  {word: "schema", definition: "Similar to an object constructor, this thingie is a diagram or blueprint for what every object in the noSQL database will contain."},
+  {word: "model", definition: "This thingie is a Schema that has been 'activated' with real data and is performing actions such as reading, saving, updating, etc."},
+  {word: "node.js", definition: "a tool to run JavaScript outside the browser, directly on your OS."},
+  {word: "rest", definition: "this acronym is a software architectural style for building scalable and performant web services."}
+]
 
 //configure//
 //serve js & css files
-app.use('/static' express.static('public'));
+app.use('/static', express.static('public'));
 app.use('/vendor', express.static('bower_components'));
 //body parser config to accept all data types
-app.use(bodyParser.urlencoded({ extend: true}));
+//app.use(bodyParser.urlencoded({ extend: true}));
 
+//routes//
 
 
 
@@ -23,7 +32,12 @@ app.use(bodyParser.urlencoded({ extend: true}));
 app.get("/", function (req, res) {
     // send back the response: 'Hello World'
     res.send("Hello World");
+    res.sendFile(path.join(views + 'index.html'));
 });
+
+app.get('/phrases', function (req, res){
+	res.send(phrases);	
+}),
 
 // start the server
 app.listen(3333, function () {
